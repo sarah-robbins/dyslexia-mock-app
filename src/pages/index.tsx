ad9e99d676012cd25854e474f18d66003d7e0a4c
Sarah Robbins <sarah@covalentdesigns.com>
2025-08-23 23:14:56 -0500

    auto redirect to dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 import { type NextPage } from "next";
 import Head from "next/head";
 import { Card } from "primereact/card";
 
 const Home: NextPage = () => {
-  // const router = useRouter();
-  // const [selectedUserId, setSelectedUserId] = useState<number | null>(null);
+  const router = useRouter();
 
-  // useEffect(() => {
-    // Check if user was previously selected
-  //   const savedUserId = localStorage.getItem('demoUserId');
-  //   if (savedUserId) {
-  //     setSelectedUserId(parseInt(savedUserId));
-  //   }
-  // }, []);
-
-  // const handleUserSelect = (userId: number) => {
-  //   setSelectedUserId(userId);
-  // };
-
-  // const enterDashboard = () => {
-  //   if (selectedUserId) {
-  //     void router.push('/dashboard');
-  //   }
-  // };
+  useEffect(() => {
+    // Automatically redirect to dashboard
+    void router.push('/dashboard');
+  }, [router]);
 
   return (
     <>
